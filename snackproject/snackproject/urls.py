c3b46f311ed9a377783750b4efa9e13ac429ecd7
seralv <sergioalvaro.14@gmail.com>
2023-07-26 06:23:13 -0400

    change url

--- snackproject/snackproject/urls.py
+++ snackproject/snackproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snackapi/', include('snackapi.urls')),
+    path('', include('snackapi.urls')),
 ]
